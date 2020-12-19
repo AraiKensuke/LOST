cbdf0b21f62bffa43e48ea014446956cd616968c
arai <kensuke.y.arai@gmail.com>
2020-12-19 06:03:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ from distutils.extension import Extension
 #from Cython.Build import cythonize      # cythonize compiles a pyx
 from Cython.Distutils import build_ext   # Extension for a c-file, build_ext for cython file
 
-#modules = ["hc_bcast", "par_intgrls_f", "par_intgrls_q2", "fastnum"]
-#modules = ["kfARlibMPmv_ram2", "kfcomMPmv_ram"]
-modules = ["kfARlib1c"]
+modules = ["kfARlibMPmv_ram2", "kfcomMPmv_ram", "kfARlib1c"]
 
 ###  import LogitWrapper 
 ###  LogitWrapper
